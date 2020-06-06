0c38740bc720458578f17c5eedf5625aa85c7cd6
Dhruv Kalia <dhruvkalia13@gmail.com>
2020-06-06 09:46:48 +0530

    LeetCode June(5) RandomPickWithWeight

---
+++ leetcode.june/Q528RandomPickWithWeight.py
@@ -0,0 +1,22 @@
+import bisect
+import random
+
+
+class Solution:
+    cumSum = []
+
+    def __init__(self, w: [int]):
+        # w = w[0]
+        self.cumSum = [w[0]]
+        for i in range(1, len(w)):
+            self.cumSum.append(self.cumSum[i - 1] + w[i])
+        print("Cumsum is " + str(self.cumSum))
+
+    def pickIndex(self) -> int:
+        x = random.randint(1, self.cumSum[-1])
+        return bisect.bisect_left(self.cumSum, x)
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution([3,14,1,7])
+# param_1 = obj.pickIndex()
+# print(param_1)
